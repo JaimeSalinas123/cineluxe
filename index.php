5cf4d039f7b596487da2f4e16b50a906654b74d4
Jaime Lopez <jnlopezjr36@gmail.com>
2025-07-31 17:01:04 -0600

    already in cinema
    the all for the cine

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="design/index.css">
+    <title>CineLuxe</title>
+</head>
+<body>
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <a href="index.php" class="flex items-center space-x-6 rtl:space-x-reverse">
+            <img src="Cineluxe-imgs/CineLuxe-logo-removebg-preview.png" class="h-20" alt="Cineluxe logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CineLuxe</span>
+        </a>
+        <div class="flex items-center space-x-6 rtl:space-x-reverse">
+            <a href="tel:5541251234" class="self-center text-1xl font-semibold whitespace-nowrap dark:text-white">Locations</a>
+            <a href="#" class="self-center text-1xl font-semibold whitespace-nowrap dark:text-white">Login</a>
+        </div>
+    </div>
+</nav>
+
+<header>
+    <section>
+        <div id="controls-carousel" class="relative w-full" data-carousel="slide">
+            <div data-carousel-autoplay-interval="5000" class="hidden"></div>
+            <div class="relative h-[40vh] min-h-[200px] max-h-[500px] overflow-hidden rounded-lg">
+                <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+                    <img src="Cineluxe-imgs/tiposmalos-carrusel.jpg" class="absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Película 1">
+                </div>
+                <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                    <img src="Cineluxe-imgs/4fantasticos-carrusel.jpg" class="absolute block w-full h-full object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="Película 2">
+                </div>
+            </div>
+            <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+            </button>
+            <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+            </button>
+        </div>
+    </section>
+
+<section>
+        <div class="cartel">
+            <div class="cartel-section">
+                <div class="text-cartel">
+                    <h2>Already in cinema</h2>
+                </div>
+
+            <div class="cartel-card">
+                <a href="URL_DESTINO" class="card-link" target="_blank">
+                    <div class="card-text">
+                        <h2>The bad guys 2: back in badness</h2>
+                        <img src="Cineluxe-imgs/the-bad-guys-2.avif" alt="the bad guys">
+                        <p>For all ages</p>
+                        <p>120 minutes</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="cartel-card">
+                <a href="URL_DESTINO" class="card-link" target="_blank">
+                    <div class="card-text">
+                        <h2>The fantastic four: the first step</h2>
+                        <img src="Cineluxe-imgs/the-fantastic-four.jpg" alt="the bad guys">
+                        <p>For all ages</p>
+                        <p>115 minutes</p>
+                    </div>
+                </a>
+            </div>
+
+             <div class="cartel-card">
+                <a href="URL_DESTINO" class="card-link" target="_blank">
+                    <div class="card-text">
+                        <h2>Superman (2025) <br>Look up</h2>
+                        <img src="Cineluxe-imgs/superman 2025.jpg" alt="the bad guys">
+                        <p>For all ages</p>
+                        <p>130 minutes</p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="cartel-card">
+                <a href="URL_DESTINO" class="card-link" target="_blank">
+                    <div class="card-text">
+                        <h2>Formula 1 <br>la pelicula</h2>
+                        <img src="Cineluxe-imgs/f1-themovie.jpg" alt="the bad guys">
+                        <p>For all ages</p>
+                        <p>156 minutes</p>
+                    </div>
+                </a>
+            </div>
+
+            </div>
+        </div>
+    </section>
+</header>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+</body>
+</html>
