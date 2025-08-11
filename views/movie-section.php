ad205932536071d23adf735dafbb1e1d92bd2fe3
Jaime Lopez <jnlopezjr36@gmail.com>
2025-08-10 18:39:48 -0600

    movies
    movies

--- views/movie-section.php
+++ views/movie-section.php
@@ -10,7 +10,6 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/CineLuxe/design/movie-section2.css">
-    <script src="https://js.stripe.com/v3/"></script>
     <title>CineLuxe</title>
 </head>
 <body>
